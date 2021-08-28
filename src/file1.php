d2194ed786ae5b6152d5151610c2525863aadb6c
Umberto Loria JESSE <umberto.loria@gmail.com>
2021-08-28 18:56:46 +0200

    Improved bad code template 2

--- src/file1.php
+++ src/file1.php
@@ -30,11 +30,47 @@ class file1{
 {return$a + $b; }echo sum(1, 2); echo "\n"; $a = 5; if (5 === $a) { echo 'yea'; }
 
 
-function foo( )
+function foo( $x)
 {
     if ($x > 5) {
         echo "bar";
     }return
         "string"; }
+
+
+class Foo
+{  function foo ( $x  , $z ){
+        global$k,$s1;
+         $this->foo(x: 1)->bar();
+        $arr= array(0 => "zero", 1 => "one");
+        call_func(function() {
+            return 0;
+
+        });
+        call_func(fn() => 0);
+        for ($i = 0; $i < $x; $i++) {
+             $y += ($y ^ 0x123) << 2; }
+        $k = $x > 15 ? 1 : 2; $k = $x ?: 0;
+        $k = $x ?? $z; $k= $x<=> $z;
+        do{
+            try{ if(!0> $x &&!$x < 10) {
+           while ($x!= $y) { $x = f($x * 3 + 5); }
+                $z += 2;
+        } elseif ($x > 20) { $z = $x << 1; } else {
+ $z = $x | 2;      }
+   $j = (int)$z; switch( $j){ case 0:
+                        $s1 = 'zero'; break;
+                    case 2: $s1 = 'two'; break;
+                default:
+      $s1 = 'other';
+                } } catch (exception$e){$t = $e[0];$u = $t['str'];
+                $v = $s1[$x[1]]; echo $x;
+  } finally {
+          // do something
+        }} while ($x < 0);
+    }}
+function bar(): void{ return; }
+
+
 
     ?>
