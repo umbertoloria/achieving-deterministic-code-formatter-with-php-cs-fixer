6eafa7f3db3886cadb9373ac8b1e5e469c161446
Umberto Loria JESSE <umberto.loria@gmail.com>
2021-08-28 21:35:21 +0200

    Improved bad code template 5

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -6,6 +6,9 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@PhpCsFixer' => true,
         'concat_space' => ['spacing' => 'one'],
+        'cast_spaces' => ['space' => 'none'],
+        'function_declaration' => ['closure_function_spacing' => 'one'],
+        'no_closing_tag' => true,
         // risky rules
         'declare_strict_types' => true,
     ])
