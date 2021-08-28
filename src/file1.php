2921fa16f3922a77592ee385fe1348c86e496284
Umberto Loria JESSE <umberto.loria@gmail.com>
2021-08-28 17:10:10 +0200

    Bad code

--- src/file1.php
+++ src/file1.php
@@ -1,1 +1,32 @@
 <?php
+
+$coseACaso = array(
+    'ciao' =>  5,
+    4 => 33,
+    1=> [
+        'ciao', 'a',
+        'tutti'
+    ],
+    2=> [1, 2],
+    3 => [
+        1,
+        2,
+        3
+        ,
+        4,
+    ],
+    8 => new stdClass()
+);
+class A {
+    public function ciao() {
+        echo 'ciao';
+        return 1;
+    }
+    public function aoic() {
+        $a = 4;
+        return$a;
+    }
+}
+function x() {
+    echo '2';
+}
